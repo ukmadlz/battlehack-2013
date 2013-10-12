269a90b961cc5b6aa94a6c05fe9c06ad91189185
Hugh Rawlinson <hugh@codeoclock.net>
2013-10-13 00:57:55 +0100

    merged some shite

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var pp = require('../lib/paypal.js')
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.authorize = function(req,res){
+	callbacks = {
+        authorizePaymentError: function(err){
+            res.json({
+            	status: 'error',
+            	data: err
+            });
+        },
+        authorizePaymentSuccess: function(data){
+            res.json({
+            	status: 'not error',
+            	data: data
+            });
+        }
+    }
+	pp.authorizePayment(callbacks);
+}
+
+exports.echo = function(req,res){
+	res.json(req.body);
+}
