1ad7cf9164cb5157173f5caae0b4c6bdc3743ab5
Hugh Rawlinson <hugh@codeoclock.net>
2013-10-13 01:20:18 +0100

    added other files

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var config = require('config');
+var paypal_sdk = require('paypal-rest-sdk');
+
+paypal_sdk.configure(config.paypal);
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 5000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/authorize', routes.authorize);
+app.get('/echo', routes.echo);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ config/default.js
@@ -0,0 +1,8 @@
+module.exports = {
+	paypal:{
+		'host': 'api.sandbox.paypal.com',
+    	'port': '',
+    	'client_id': 'AcwLkhD8rIMtWmGKqxBU1qVP4y3Fc2DJGcIeGYUPL3zTZlEPYiEgMQ3LOccO',
+    	'client_secret': 'EI0jJBAQBA0vHQz9ViWyctsWofSrO5X_PCd7AOKm7n4aYPVJWq-Id3x0FFY5'
+	}
+}
